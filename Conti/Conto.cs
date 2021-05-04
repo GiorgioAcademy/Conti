181cc4fee5834dcfc8d0a7da7c957a0c81c85b37
Giorgio Vecchi <gvecchi@hotmail.com>
2021-05-04 15:56:49 +0200

    Add project files.

---
+++ Conti/Conto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conti
+{
+    class Conto
+    {
+        public decimal Saldo { get; private set; }   // proprietà implcitamente definita
+        public string Intestatario { get; }
+        public int Numero { get; }  // il numero del conto in questione
+
+        private static int _num;
+
+        //private decimal _saldo;
+
+        //public decimal Saldo
+        //{
+        //    get { return _saldo; }
+        //    set { _saldo = value; }
+        //}
+
+        public Conto(string intestatario)
+        {
+            Numero = ++_num;
+
+            Intestatario = intestatario;
+        }
+
+        public void Versa(decimal importo)
+        {
+            if (importo < 0)
+                throw new ArgumentOutOfRangeException(nameof(importo), 
+                    "L'importo non può essere negativo");
+
+            Saldo += importo;
+        }
+
+        public void Preleva(decimal importo)
+        {
+            if (importo < 0)
+                throw new ArgumentOutOfRangeException(nameof(importo),
+                    "L'importo non può essere negativo");
+
+            Saldo -= importo;
+        }
+    }
+}
---
+++ Conti/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Conti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random r = new Random();
+
+            //r.Next(1, 91);
+
+            RandomSR r = new RandomSR(1, 91);
+
+            while (r.Rimasti > 0)
+                Console.WriteLine(r.Next);
+
+            Conto c1 = new Conto("Pippo");
+            Conto c2 = new Conto("Pluto");
+
+            Conto[] conti = new Conto[100];
+
+            conti[0] = c1;
+            conti[1] = c2;
+            conti[2] = new Conto("Pinco");
+
+            c1.Versa(100.50m);
+            c1.Versa(150);
+
+            c2.Versa(500);
+            c1.Preleva(100);
+
+            c2.Preleva(50);
+
+            // c1.Saldo = 1000000;
+
+            Console.WriteLine($"Il saldo del conto di {c1.Intestatario} è {c1.Saldo}");
+            Console.WriteLine($"Il saldo del conto di {c2.Intestatario} è {c2.Saldo}");
+        }
+    }
+}
